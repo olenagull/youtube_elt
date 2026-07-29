9889df7f7c3073d5dcedd3a56fa01aad2d88237b
olenagull <olena.gull@gmail.com>
2026-07-29 13:33:38 +0300

    Added DAG

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,19 +2,22 @@ import requests
 import json
 from datetime import date
 
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv('API_KEY')
-CHANNEL_HANDLE = 'MrBeast'
+API_KEY = Variable.get('API_KEY')
+CHANNEL_HANDLE = Variable.get('CHANNEL_HANDLE')
 max_results = 50
 
 # print(f"API_KEY: {API_KEY}")
 # print(f"Length: {len(API_KEY) if API_KEY else 'None'}")
 # print(os.getcwd())
 
+@task
 def get_playlist_id():
 
     try:
@@ -38,7 +41,7 @@ def get_playlist_id():
         raise e
 
 
-
+@task
 def get_video_ids(playlist_id):
 
     video_ids = []
@@ -75,6 +78,7 @@ def get_video_ids(playlist_id):
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -120,6 +124,7 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
             raise e
 
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
 
