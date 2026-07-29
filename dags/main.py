96c46de00a8353b0f8836dad1e1486f9629781c8
olenagull <olena.gull@gmail.com>
2026-07-29 14:51:37 +0300

    added data insert and modification

--- dags/main.py
+++ dags/main.py
@@ -1,7 +1,13 @@
 from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
-from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
+from api.video_stats import (
+    get_playlist_id,
+    get_video_ids,
+    extract_video_data,
+    save_to_json,
+)
+from datawarehouse.dwh import staging_table, core_table
 
 # Define the local timezone
 local_tz = pendulum.timezone("Europe/London")
@@ -21,13 +27,18 @@ default_args = {
     # 'end_date': datetime(2030, 12, 31, tzinfo=local_tz),
 }
 
+# Variables
+staging_schema = "staging"
+core_schema = "core"
+
+# DAG 1: produce_json
 with DAG(
     dag_id='produce_json',
     default_args=default_args,
     description='DAG to produce JSON file with raw data',
     schedule='0 14 * * *',
     catchup=False
-) as dag:
+) as dag_produce:
 
     # Define tasks
     playlist_id = get_playlist_id()
@@ -37,3 +48,20 @@ with DAG(
 
     # Define dependancies
     playlist_id >> video_ids >> extract_data >> save_to_json
+
+# DAG 2: update_db
+with DAG(
+    dag_id='update_db',
+    default_args=default_args,
+    description='DAG to process JSON file and insert data into staging and core schemas',
+    schedule='0 15 * * *',
+    catchup=False,
+    # schedule=None,
+) as dag_update:
+
+    # Define tasks
+    update_staging = staging_table()
+    update_core = core_table()
+
+    # Define dependancies
+    update_staging >> update_core 
